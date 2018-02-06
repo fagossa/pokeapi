531b028b8a2a46718b08dca5459bbea80374776c
Fabian Gutierrez <fagossa@gmail.com>
2018-02-06 21:43:55 +0100

    add a new component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ShoppingList from './ShoppingList';
+import {Square, Board, Game} from './Game';
 
 /* eslint-disable */
 
-class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
-}
-
 ReactDOM.render(
-  <HelloMessage name="John" />,
+  <ShoppingList />,
   document.getElementById('root')
 );
